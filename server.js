e0ce093d816130ed639c9d389c47acc7cbe1d792
Tamara Hussien <tamarahussien97@gmail.com>
2020-10-25 16:38:06 +0300

    add try and catch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let cors = require('cors');
+const { response } = require('express');
 
 let app = express();
 app.use(cors());
@@ -10,13 +11,21 @@ const PORT = process.env.PORT;
 
 app.get('/location',handleLocation);
 
+
 function handleLocation(request,response){
-    let city = request.query.city;
-    let jsonData = require('./data/location.json')
-    let jsonObject = jsonData[0];
-    let locationObject = new Location(city,jsonObject.display_name,jsonObject.lat,jsonObject.lon);
-    response.status(200).json(locationObject);
+    try{
+        let city = request.query.city;
+        let jsonData = require('./data/location.json')
+        let jsonObject = jsonData[0];
+        let locationObject = new Location(city,jsonObject.display_name,jsonObject.lat,jsonObject.lon);
+        response.status(200).json(locationObject);
+    }
+    catch(error){
+        response.status(500).send('Sorry, something went wrong');
+    }
 }
+
+
 
 function Location(search_query,formatted_query,latitude,longitude){
     this.search_query = search_query;
